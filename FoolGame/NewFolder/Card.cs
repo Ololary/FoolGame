727552268d8039df8cfe6eb5f81374ce6924f79b
DESKTOP-98NGS9V\Александр <oshino.shinobu@yandex.ru>
2022-12-16 01:48:18 +0300

    Теперь карта это тип, описанный в классе Card, а колода это список из карт. Добавил методы для вывода карт на консоль а также метод для раздачи карт

---
+++ FoolGame/NewFolder/Card.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainingApp
+{
+    internal class Card
+    {
+        public bool trumpvalue = false;
+        public enum Suit
+        {
+            Крести = 1,
+            Черви = 2,
+            Пики = 3,
+            Бубны = 4,
+        }
+        public enum Value
+        {
+            Шесть = 6,
+            Семь = 7,
+            Восемь = 8,
+            Девять = 9,
+            Десять = 10,
+            Валет = 11,
+            Дама = 12,
+            Король = 13,
+            Туз = 14
+        }
+        public Card(Value cardValue, Suit cardSuit)
+        {
+            CardSuit = cardSuit;
+            CardValue = cardValue;
+        }
+
+        public Suit CardSuit;
+        public Value CardValue;
+        public string ShowCard(Card card)
+        {
+            return card.CardValue.ToString() + " " + card.CardSuit.ToString();
+        }
+
+    }
+}
---
+++ FoolGame/NewFolder/GameStart.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TrainingApp;
+
+namespace FoolGame.NewFolder
+{
+    internal class GameStart
+    {
+        public List<Card> deck = new List<Card>()
+{
+    new Card(Card.Value.Шесть,Card.Suit.Крести),
+    new Card(Card.Value.Шесть,Card.Suit.Черви),
+    new Card(Card.Value.Шесть,Card.Suit.Пики),
+    new Card(Card.Value.Шесть,Card.Suit.Бубны),
+
+    new Card(Card.Value.Семь,Card.Suit.Крести),
+    new Card(Card.Value.Семь,Card.Suit.Черви),
+    new Card(Card.Value.Семь,Card.Suit.Пики),
+    new Card(Card.Value.Семь,Card.Suit.Бубны),
+
+    new Card(Card.Value.Восемь,Card.Suit.Крести),
+    new Card(Card.Value.Восемь,Card.Suit.Черви),
+    new Card(Card.Value.Восемь,Card.Suit.Пики),
+    new Card(Card.Value.Восемь,Card.Suit.Бубны),
+
+    new Card(Card.Value.Девять,Card.Suit.Крести),
+    new Card(Card.Value.Девять,Card.Suit.Черви),
+    new Card(Card.Value.Девять,Card.Suit.Пики),
+    new Card(Card.Value.Девять,Card.Suit.Бубны),
+
+    new Card(Card.Value.Десять,Card.Suit.Крести),
+    new Card(Card.Value.Десять,Card.Suit.Черви),
+    new Card(Card.Value.Десять,Card.Suit.Пики),
+    new Card(Card.Value.Десять,Card.Suit.Бубны),
+
+    new Card(Card.Value.Валет,Card.Suit.Крести),
+    new Card(Card.Value.Валет,Card.Suit.Черви),
+    new Card(Card.Value.Валет,Card.Suit.Пики),
+    new Card(Card.Value.Валет,Card.Suit.Бубны),
+
+    new Card(Card.Value.Дама,Card.Suit.Крести),
+    new Card(Card.Value.Дама,Card.Suit.Черви),
+    new Card(Card.Value.Дама,Card.Suit.Пики),
+    new Card(Card.Value.Дама,Card.Suit.Бубны),
+
+    new Card(Card.Value.Король,Card.Suit.Крести),
+    new Card(Card.Value.Король,Card.Suit.Черви),
+    new Card(Card.Value.Король,Card.Suit.Пики),
+    new Card(Card.Value.Король,Card.Suit.Бубны),
+
+    new Card(Card.Value.Туз,Card.Suit.Крести),
+    new Card(Card.Value.Туз,Card.Suit.Черви),
+    new Card(Card.Value.Туз,Card.Suit.Пики),
+    new Card(Card.Value.Туз,Card.Suit.Бубны),
+};
+        public List<Card> playerhand = new List<Card>();
+        public List<Card> brainhand = new List<Card>();
+        Random random = new Random();
+        public void CardToHands()
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                var rand = deck[random.Next(deck.Count)];
+                deck.Remove(rand);
+                playerhand.Add(rand);
+            }
+            for (int i = 0; i < 6; i++)
+            {
+                var rand = deck[random.Next(deck.Count)];
+                deck.Remove(rand);
+                brainhand.Add(rand);
+            }
+        }
+        public void ShowHand(List<Card> hand)
+        {
+            foreach (var item in hand)
+            {
+                Console.WriteLine(item.ShowCard(item));
+            }
+
+        }
+
+        public void MakeATrump()
+        {
+            Console.WriteLine("Достаю козырную карту...");
+            var trump = deck[random.Next(deck.Count)];
+            Console.WriteLine(trump.ShowCard(trump)+"!!");
+            Console.WriteLine($"Козырь {trump.CardSuit}!!");
+        }
+    }
+}
